7c3975b167ee19c2de7f008409acbdb60477ee21
YoniWitz <jonhirsh39@gmail.com>
2024-09-17 10:33:24 -0400

    added filter

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
-import { catchError, EMPTY, forkJoin, map, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, EMPTY, map, Observable } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
-
 import { Product } from './product';
 import { ProductService } from './product.service';
 import { ProductCategoryService } from '../product-categories/product-category.service';
@@ -15,44 +13,69 @@ import { ProductCategoryService } from '../product-categories/product-category.s
 export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
-  combined$: Observable<Product[]> | undefined;
+  //combined$: Observable<Product[]> | undefined;
+  combinedFiltered$: Observable<Product[]> | undefined;
   products$: Observable<Product[]> | undefined;
   productCategories$: Observable<ProductCategory[]> | undefined;
 
-  constructor(private productService: ProductService, private productCategoryService : ProductCategoryService) { }
+  private selectedFilteredSubject = new BehaviorSubject<number>(0);
+  selectedFilterAction$: Observable<number> | undefined;
+
+  constructor(private productService: ProductService, private productCategoryService: ProductCategoryService) { }
 
   ngOnInit(): void {
+    this.selectedFilterAction$ = this.selectedFilteredSubject.asObservable();
+
     this.products$ = this.productService.products$.
-    pipe(
-      catchError(err => {
-        this.errorMessage = err;
-        return EMPTY;
-      })
-    );
+      pipe(
+        catchError(err => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      );
 
     this.productCategories$ = this.productCategoryService.productCategories$.
-    pipe(
+      pipe(
+        catchError(err => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      );
+
+    // this.combined$ = forkJoin([this.products$, this.productCategories$]).pipe(
+    //   map(([products, categories]) =>
+    //     products.map(product => ({
+    //       ...product,
+    //       categoryName: categories.find(category => category.id === product.categoryId)?.name
+
+    //     } as Product)))
+    // );
+
+    this.combinedFiltered$ = combineLatest([
+      this.products$, this.productCategories$, this.selectedFilterAction$
+    ]).pipe(
+      map(([products, categories, categoryId]) => {
+        let productCategories = products.map(product => ({
+          ...product,
+          categoryName: categories.find(category => category.id === product.categoryId)?.name
+
+        } as Product));
+        return productCategories.filter(product => categoryId ? product.categoryId === categoryId : true)
+      }),
       catchError(err => {
         this.errorMessage = err;
         return EMPTY;
       })
-    );
 
-    this.combined$ = forkJoin([this.products$, this.productCategories$]).pipe(
-      map(([products,categories]) =>
-        products.map(product =>({
-            ...product,
-            categoryName: categories.find(category => category.id===product.categoryId)?.name
-
-        } as Product)))
-    );
+    )
   }
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
-  onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+  onSelected(categoryId: number): void {
+    this.selectedFilteredSubject.next(+categoryId);
   }
+
 }
